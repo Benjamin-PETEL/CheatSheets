e4545d29b9f446f9be2bb5da2e32c637e5be19bb
Benjamin PETEL <benjamin.petel@outlook.com>
2021-10-22 21:58:56 +0200

    insert first template

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+
+// Init app
 const app = express();
 
+// Templating
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// Routing
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.render('index');
 });
 
 app.listen(3000, () => {
-    console.log('app running on port 3000');
+    console.log('express running on port 3000');
 });
