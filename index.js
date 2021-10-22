3f342650d239f638a45fbdde404a2eec8af10517
Benjamin PETEL <benjamin.petel@outlook.com>
2021-10-22 22:35:01 +0200

    add hard coded articles

--- index.js
+++ index.js
@@ -7,9 +7,30 @@ const app = express();
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+// TEMP
+const articles = [
+    {
+        'title': '1',
+        'content': 'detail article 1'
+    },
+    {
+        'title': '2',
+        'content': 'detail article 2'
+    },
+    {
+        'title': '3',
+        'content': 'detail article 3'
+    },
+]
+
 // Routing
 app.get('/', (req, res) => {
     res.render('index');
+});
+articles.forEach(article => {
+    app.get('/'+article.title, (req, res) => {
+        res.render('article', article);
+    })
 });
 
 app.listen(3000, () => {
